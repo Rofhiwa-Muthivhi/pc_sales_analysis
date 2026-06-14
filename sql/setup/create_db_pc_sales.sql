23f9454c4a6e064aa51f8894d630b7ef845263ab
Rofhiwa - Analyst <muthivhi0207@gmail.com>
2026-06-14 14:17:20 +0200

    Restructuring the project to data analytics foundation as phase_01

--- sql/setup/create_db_pc_sales.sql
+++
@@ -1,5 +0,0 @@
--- For this project 2 databases are created for the purpose of storing raw uncleaned data
--- Computer_std - staging, this is where raw data will be stored
--- Computer_dhw - backup database for upcoming tasks
-CREATE DATABASE Computer_std
-CREATE DATABASE Computer_dhw
